33f95145d76dbdd6b44932ef18e1806af202bb96
wojiaoyue <2391301541@qq.com>
2018-06-24 15:21:45 +0800

    vue

---
+++ mock/sliders.js
@@ -0,0 +1,7 @@
+module.exports = [
+
+  "http://p5.so.qhimgs1.com/t01646e1558d3def0cb.jpg",
+  "http://p1.so.qhimgs1.com/sdr/200_200_/t018975890334926221.jpg",
+  "http://p0.so.qhimgs1.com/t015a59f8713cc11340.jpg"
+
+]
